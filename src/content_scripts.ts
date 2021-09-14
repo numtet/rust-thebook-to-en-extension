0fb0150f19c15fa43d14646e55bef83bbfc9bc8a
numtet <tet.namura@gmail.com>
2021-09-15 01:12:11 +0900

    Add link element

--- src/content_scripts.ts
+++ src/content_scripts.ts
@@ -1,13 +1,24 @@
-import { browser } from 'webextension-polyfill-ts'
-
-console.log('from content_scripts')
-
-const execute = async () => {
-  const value = await browser.storage.local.get('date')
-  console.log(value.date || '日時が記録されていません')
-
-  await browser.storage.local.set({ date: new Date().toString() })
-  console.log('現在の日時を記録しました')
+const createEnLink = (jpLink: string) => {
+  return jpLink
+    .replace('doc.rust-jp.rs', 'doc.rust-lang.org')
+    .replace(/\/([^\/]*)-ja\//, (_match, p1) => `/${p1}/`)
 }
 
-execute()
+const addLinkNode = () => {
+  const link = document.createElement('a')
+  link.href = createEnLink(window.location.toString())
+  link.title = "Go to English page"
+  link.ariaLabel = "Go to English page"
+  link.target = "_blank"
+
+  const icon = document.createElement('i')
+  icon.id = "english-page-button"
+  icon.className = "fa fa-language"
+
+  link.appendChild(icon)
+
+  const buttons = document.querySelector('div.right-buttons')
+  buttons?.appendChild(link)
+}
+
+addLinkNode()
