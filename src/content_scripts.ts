584cdc01db897fb8e26ebf0118485b015a272722
numtet <tet.namura@gmail.com>
2021-09-15 00:14:33 +0900

    Typescript settings

---
+++ src/content_scripts.ts
@@ -0,0 +1,13 @@
+import { browser } from 'webextension-polyfill-ts'
+
+console.log('from content_scripts')
+
+const execute = async () => {
+  const value = await browser.storage.local.get('date')
+  console.log(value.date || '日時が記録されていません')
+
+  await browser.storage.local.set({ date: new Date().toString() })
+  console.log('現在の日時を記録しました')
+}
+
+execute()
